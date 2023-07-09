4ebd33b326b90a3fdd504e1cba2e412a8fa58200
Dener Garcia <mundoem3di@gmail.com>
2023-07-09 17:59:41 +0000

    refact: improve javascript function change images

--- src/app.js
+++ src/app.js
@@ -1,82 +1,46 @@
-const btnWhite = document.querySelector("#white")
-const btnBlack = document.querySelector("#black")
-const btnBlue = document.querySelector("#blue")
-const btnOrange = document.querySelector("#orange")
-const btnPink = document.querySelector("#pink")
-const btnGrey = document.querySelector("#grey")
-const btnGold = document.querySelector("#gold")
+
+const btnColors = document.querySelectorAll(".tune-colors input[name='color']");
 const productImg = document.querySelector(".product img")
 
-function changeImg(path) {
- 
-    productImg.setAttribute("src", path)
-    productImg.classList.add("fadeJS")
+function changeImg(pathImg, frameColor, boardColor, ) {
+    productImg.setAttribute("src", pathImg)
     productImg.style.filter = "grayscale(0)"
+    productImg.setAttribute("alt", "Placa com QR Code" + " " + frameColor + " " + "personalizada" + " " + "com detalhes em" + " " + boardColor)
+    
 
-    setTimeout(() => {
-        productImg.classList.remove("fadeJS")
-    }, 200);
 }
 
-btnWhite.addEventListener("click", () => {
-    changeImg(path = "/public/orange.opti.webp")
-    productImg.style.filter = "grayscale(1)"
+for (let detailColor of btnColors) {
 
-    btnBlue.addEventListener("click", () => {
-        changeImg(path = "/public/azul-off.opti.webp")
-        btnWhite.focus()
-    })
+    detailColor.addEventListener('change', function (event) {
 
-    btnOrange.addEventListener("click", () => {
-        changeImg(path = "/public/orange.opti.webp")
-        btnWhite.focus()
-    })
+        let boardColor = document.querySelector("input[name='tone']:checked");
+        if (!boardColor) {
+            alert('Ops, Escolha a cor da placa antes 😉');
 
-    btnPink.addEventListener("click", () => {
-        changeImg(path = "/public/pink.opti.webp")
-        btnWhite.focus()
-    })
+            return;
+        }
 
-    btnGrey.addEventListener("click", () => {
-        changeImg(path = "/public/grey.opti.webp")
-        btnWhite.focus()
-    })
+        switch (boardColor.value) {
+            case "white":
+                changeImg(
+                    pathImg = detailColor.dataset.toneWhite,
+                    frameColor = "Branca" ,
+                    boardColor = detailColor.ariaLabel
+                    )
+                break;
+            case "black":
+                changeImg(
+                    pathImg = detailColor.dataset.toneBlack,
+                    frameColor = "Preta" ,
+                    boardColor = detailColor.ariaLabel
+                    )
+            default:
+                break;
+        }
+    });
+}
 
-    btnGold.addEventListener("click", () => {
-        changeImg(path = "/public/gold.opti.webp")
-        btnWhite.focus()
-    })
-})
-
-btnBlack.addEventListener("click", () => {
-    changeImg(path = "/public/orange-dark.opti.webp")
-    productImg.style.filter = "grayscale(1)"
-
-    btnBlue.addEventListener("click", () => {
-        changeImg(path = "/public/azul-off-dark.opti.webp")
-        btnBlack.focus()
-    })
-
-    btnOrange.addEventListener("click", () => {
-        changeImg(path = "/public/orange-dark.opti.webp")
-        btnBlack.focus()
-    })
-
-    btnPink.addEventListener("click", () => {
-        changeImg(path = "/public/pink-dark.opti.webp")
-        btnBlack.focus()
-    })
-
-    btnGrey.addEventListener("click", () => {
-        changeImg(path = "/public/grey-dark.opti.webp")
-        btnBlack.focus()
-    })
-
-    btnGold.addEventListener("click", () => {
-        changeImg(path = "/public/gold-dark.opti.webp")
-        btnBlack.focus()
-    })
-})
 
 // animations
 
@@ -84,56 +48,56 @@ const reveralUP = document.querySelectorAll(".animation-up")
 const reveralLeft = document.querySelectorAll(".animation-left")
 const reveralGirl = document.querySelectorAll(".girlAnimation")
 
-const animationUP = () =>{
+const animationUP = () => {
 
-reveralUP.forEach((e) => {
-    let windowHeight = window.innerHeight
-    let elementAtTop = e.getBoundingClientRect().top
-    let elementVisible = 10
+    reveralUP.forEach((e) => {
+        let windowHeight = window.innerHeight
+        let elementAtTop = e.getBoundingClientRect().top
+        let elementVisible = 10
 
-    if (elementAtTop < windowHeight - elementVisible) {
-        e.classList.add("reveral-up")
-    } else {
-        e.classList.remove("reveral-up")
-    }
-});
+        if (elementAtTop < windowHeight - elementVisible) {
+            e.classList.add("reveral-up")
+        } else {
+            e.classList.remove("reveral-up")
+        }
+    });
 }
 
 window.addEventListener("scroll", animationUP)
 
-const animationLeft = () =>{
+const animationLeft = () => {
 
     reveralLeft.forEach((e) => {
         let windowHeight = window.innerHeight
         let elementAtTop = e.getBoundingClientRect().top
         let elementVisible = 10
-    
+
         if (elementAtTop < windowHeight - elementVisible) {
             e.classList.add("reveral-left")
         } else {
             e.classList.remove("reveral-left")
         }
     });
-    }
-    window.addEventListener("scroll", animationLeft)
+}
+window.addEventListener("scroll", animationLeft)
 
-    const animationGirl = () =>{
+const animationGirl = () => {
 
-        reveralGirl.forEach((e) => {
-            let windowHeight = window.innerHeight
-            let elementAtTop = e.getBoundingClientRect().top
-            let elementVisible = 200
-        
-            if (elementAtTop < windowHeight - elementVisible) {
-                e.classList.add("girlAnimationOn")
-            } else {
-                e.classList.remove("girlAnimationOn")
-            }
-        });
+    reveralGirl.forEach((e) => {
+        let windowHeight = window.innerHeight
+        let elementAtTop = e.getBoundingClientRect().top
+        let elementVisible = 200
+
+        if (elementAtTop < windowHeight - elementVisible) {
+            e.classList.add("girlAnimationOn")
+        } else {
+            e.classList.remove("girlAnimationOn")
         }
+    });
+}
 
-        window.addEventListener("scroll", animationGirl)
-    
+window.addEventListener("scroll", animationGirl)
+
 
 
 
